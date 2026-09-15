338488927f01520328aebaa5c48bc9c7d0f6a96a
nattkreatur <oskar.boberg@gmail.com>
2026-09-15 16:58:39 +0200

    Removed debuglog
    Ops, I forgot a debug log. Removed it.

--- pages/chessboard.js
+++ pages/chessboard.js
@@ -4,7 +4,6 @@ let grid = "";
 
 for (let number1 = 0; number1 < bindingSize; number1++){
     // modulus % is used to see if row+column is even or uneven thus alternates between " " and "#"
-    console.log(number1);
     for (let number2 = 0; number2 < bindingSize; number2++){
         if((number1+number2) % 2 == 0){
             grid +=" ";
