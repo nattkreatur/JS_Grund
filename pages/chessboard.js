e43cbd1b122ff2ac850ffb7e6f1ce2edc138947c
nattkreatur <oskar.boberg@gmail.com>
2026-09-15 13:59:55 +0200

    Added comment
    Added a descriptive comment

--- pages/chessboard.js
+++ pages/chessboard.js
@@ -4,6 +4,7 @@ let stringFirst = " #";
 let stringSecond = "# ";
 
 for (let number = bindingSize++; number > 0; number--){
+    // modulus % is used differentiate between even and uneven numbered rows
     if(number % 2 == 0){
         console.log(stringFirst.repeat(bindingSize/2))
     }else{
